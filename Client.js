6800d6c496a72566d7d1076cfd5fd57d86de62bf
JayceH <jayceh@mail.usf.edu>
2019-01-29 12:42:37 -0500

    Added more commands and new dependencies

--- Client.js
+++ Client.js
@@ -7,6 +7,11 @@ const url = require('url');
 
 const readlineSync = require('readline-sync')
 const readline = require('readline')
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+
+const styles = JSON.parse(fs.readFileSync('style.json', 'utf8'))
 
 //Asks for username and loops until username meets requirments
 while(true) {
@@ -42,11 +47,16 @@ const readLine = readline.createInterface({
 readLine.pause
 
 webSocket.onopen = () => {
-    console.log("Connected.\n")
+    //Server connections message boxed in
+    console.log(boxen(`Welcome to the Server ${username}!\nAvailable Commands:\n(\\w + username): Whisper user with username.\n(\\i): Ask the server what your username is.\n(\\u): Get a list of server users.\n(\\l): Leave the server.`, {padding: 0}))
     readLine.resume
     readLine.on('line', (message) => {
+        //Clears readline after message is sent to get rid of clutter
+        readline.moveCursor(process.stdout, 0, -1)
+        //Whsiper command
         if(message.match(/^(\\w\s)/g))
         {
+            //Regex to get everything after \w and a space
             let whisperName = message.match(/(?<=\\w\s)(\w+)/g)
             let messageToSend = {
                 from: username,
@@ -54,8 +64,40 @@ webSocket.onopen = () => {
                 kind: "direct",
                 data: message.substr(message.indexOf(whisperName), message.length)
             }
+            console.log(`(Whispering ${whisperName[0]}): ${message.substr(message.indexOf(whisperName), message.length)}`)
             webSocket.send(JSON.stringify(messageToSend))
         }
+        //Userlist command
+        else if(message.match(/^(\\u)/g))
+        {
+            let messageToSend = {
+                from: username,
+                to: 'irrelevant',
+                kind: "userlist",
+                data: 'irrelevant'
+            }
+            webSocket.send(JSON.stringify(messageToSend))
+        }
+        //WhoAmI command
+        else if(message.match(/^(\\i)/g))
+        {
+            let messageToSend = {
+                from: username,
+                to: 'irrelevant',
+                kind: "whoami",
+                data: 'irrelevant'
+            }
+            webSocket.send(JSON.stringify(messageToSend)) 
+        }
+        //Leave the server
+        else if(message.match(/^(\\l)/g))
+        {
+            console.log(chalk.bgMagenta("Leaving server..."))
+            webSocket.close()
+            readLine.close
+            process.exit(1)
+        }
+        //Default normal message to the server
         else
         {
             let messageToSend = {
@@ -65,12 +107,30 @@ webSocket.onopen = () => {
                 data: message
             }
             webSocket.send(JSON.stringify(messageToSend))
-        }   
+        } 
     })
 
     webSocket.onmessage = msg => {
         let backMessage = JSON.parse(msg.data)
-        messageString = backMessage.from + ': ' + backMessage.data
-        console.log(messageString)
+        if(backMessage.kind === "direct")
+        {
+            console.log(`(Whisper From ${backMessage.from}): ${backMessage.data}`)
+        }
+        else if(backMessage.from === "GABServer")
+        {
+            if(backMessage.kind === "error")
+            {
+                console.log(chalk.bgRed(`ERROR: ${backMessage.data}`))
+            }
+            else
+            {
+                console.log(chalk.bgBlue(backMessage.data))
+            }
+        }
+        else
+        {
+            messageString = backMessage.from + ': ' + backMessage.data
+            console.log(messageString)
+        }
     }
 }
